1ed139743137ffa3d6b71980aec8a8821541c15d
Aman Mittal <amanmittal3305@gmail.com>
2024-11-04 21:41:45 +0530

    Learning Binary Search Tree

--- src/BinarySearchTree.java
+++ src/BinarySearchTree.java
@@ -25,6 +25,50 @@ public class BinarySearchTree {
         return root;
     }
 
+    public static boolean search(Node root, int key){
+        if(root == null){
+            return false;
+        }
+        if(root.data == key){
+            return true;
+        }
+        if(root.data > key){
+            return search(root.left,key);
+        }
+        else{
+            return search(root.right, key);
+        }
+    }
+
+    public static Node findInorderSuccessor(Node root){
+        while(root.left != null){
+            root = root.left;
+        }
+        return root;
+    }
+
+    public static Node delete(Node root, int val){
+        if(root.data < val){
+            root.right = delete(root.right, val);
+        } else if (root.data > val) {
+            root.left = delete(root.left,val);
+        }else {
+            if(root.left == null && root.right == null){
+                return null;
+            }
+            if(root.left == null){
+                return root.right;
+            } else if (root.right == null) {
+                return root.left;
+            }
+
+            Node IS = findInorderSuccessor(root.right);
+            root.data = IS.data;
+            root.right = delete(root.right, IS.data);
+        }
+        return root;
+    }
+
     public static void inorder(Node root){
         if (root == null){
             return;
@@ -35,11 +79,27 @@ public class BinarySearchTree {
     }
 
     public static void main(String[] args) {
-        int[] values = {5,1,3,4,2,7};
+        int[] values = {5, 1, 3, 4, 2, 7};
         Node root = null;
         for (int value : values) {
             root = insert(root, value);
         }
+
+        // Inorder Traversal
+//        System.out.print("Inorder Traversal: ");
+//        inorder(root);
+//        System.out.println();
+
+        // Searching for a value
+//        if (search(root, 2)) {
+//            System.out.println("Found");
+//        } else {
+//            System.out.println("Not found");
+//        }
+        inorder(root);
+        System.out.println();
+        delete(root,3);
         inorder(root);
     }
+
 }
