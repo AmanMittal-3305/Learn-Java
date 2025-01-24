1f98769848773c6dfcbea4040e03d4f20b1aa827
Aman Mittal <amanmittal3305@gmail.com>
2025-01-24 23:15:30 +0530

    Learning Graph

--- src/Bellman_Ford_Graph.java
+++ src/Bellman_Ford_Graph.java
@@ -35,12 +35,35 @@ public class Bellman_Ford_Graph {
     }
 
 
-    public static void bellmanFord(ArrayList<Edge>[] graph,int src){
+    public static void bellmanFord(ArrayList<Edge>[] graph,int src){  // O(VE)
         int[] dist = new int[graph.length];
         for(int i = 0; i < graph.length; i++){
             if(i != src){
                 dist[i] = Integer.MAX_VALUE;
             }
+        }
+
+        int V = graph.length;
+
+        for(int i = 0; i < V-1; i++){
+            //edges - O(E)
+            for(int j = 0; j < graph.length; j++){
+                for(int k = 0; k <  graph[j].size(); k++){
+                    Edge e = graph[j].get(k);
+                    int u = e.src;
+                    int v = e.dest;
+                    int wt = e.wt;
+                    //relaxation
+                    if(dist[u] != Integer.MAX_VALUE && dist[u] + wt < dist[v]){
+                        dist[v] = dist[u] + wt;
+                    }
+                }
+            }
+        }
+
+        //print
+        for(int i = 0; i < dist.length; i++){
+            System.out.print(dist[i] + " ");
         }
     }
 
@@ -48,5 +71,6 @@ public class Bellman_Ford_Graph {
         int V = 5;
         ArrayList<Edge>[] graph = new ArrayList[V];
         createGraph(graph);
+        bellmanFord(graph,0);
     }
 }
