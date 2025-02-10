a5922f1e84642aab1fd819d221f68d145353991b
Aman Mittal <amanmittal3305@gmail.com>
2025-02-10 22:19:37 +0530

    Solving DSA problem

---
+++ src/Clear_Digit.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Clear_Digit {
+    public static String clearDigits(String s) {
+        StringBuilder stack = new StringBuilder();
+        for (char c : s.toCharArray()) {
+            if (Character.isDigit(c)) {
+                if (stack.length() > 0) {
+                    stack.deleteCharAt(stack.length() - 1);
+                }
+            } else {
+                stack.append(c);
+            }
+        }
+        return stack.toString();
+    }
+}
