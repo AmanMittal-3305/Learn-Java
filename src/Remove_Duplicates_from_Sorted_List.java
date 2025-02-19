b7a9358ca62bc37de272fc7622ff4f82f09f8a96
Aman Mittal <amanmittal3305@gmail.com>
2025-02-19 21:59:17 +0530

    Solving DSA problem

---
+++ src/Remove_Duplicates_from_Sorted_List.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+
+public class Remove_Duplicates_from_Sorted_List {
+    static class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+    public ListNode deleteDuplicates(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        ListNode temp = head;
+
+        while (temp.next != null) {
+            if (temp.val == temp.next.val) {
+                temp.next = temp.next.next;  // Skip the duplicate node
+            } else {
+                temp = temp.next;  // Move forward
+            }
+        }
+
+        return head;
+    }
+}
