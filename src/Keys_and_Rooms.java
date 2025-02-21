879ce05ac092f535ec3ca92165490027f0e655cc
Aman Mittal <amanmittal3305@gmail.com>
2025-02-21 22:07:00 +0530

    Solved DSA problem using Graph

---
+++ src/Keys_and_Rooms.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Keys_and_Rooms {
+    public boolean canVisitAllRooms(List<List<Integer>> rooms) {
+        boolean[] visited = new boolean[rooms.size()];
+        dfs(rooms, 0, visited);
+
+        for (boolean v : visited) {
+            if (!v) return false;
+        }
+        return true;
+    }
+
+    private void dfs(List<List<Integer>> rooms, int room, boolean[] visited) {
+        visited[room] = true;
+        for (int key : rooms.get(room)) {
+            if (!visited[key]) {
+                dfs(rooms, key, visited);
+            }
+        }
+    }
+}
