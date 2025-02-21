9d4a8cc50e879f42112d21f455e66cdecb75d303
Aman Mittal <amanmittal3305@gmail.com>
2025-02-21 21:07:23 +0530

    Learning Graph

--- src/Cheapest_Flights_Within_K_Stops.java
+++ src/Cheapest_Flights_Within_K_Stops.java
@@ -13,10 +13,10 @@ public class Cheapest_Flights_Within_K_Stops {
         for (int i = 0; i < graph.length; i++){
             graph[i] = new ArrayList<>();
         }
-        for(int i = 0; i < flights.length; i++){
-            int src = flights[i][0];
-            int dest = flights[i][1];
-            int wt = flights[i][2];
+        for (int[] flight : flights) {
+            int src = flight[0];
+            int dest = flight[1];
+            int wt = flight[2];
 
             Edge e = new Edge(src, dest, wt);
             graph[src].add(e);
@@ -57,17 +57,22 @@ public class Cheapest_Flights_Within_K_Stops {
                 int v = e.dest;
                 int wt = e.wt;
 
-                if(dist[u] != Integer.MAX_VALUE && dist[u] + wt < dist[v] && curr.stops <= k){
+                if( curr.cost + wt < dist[v] && curr.stops <= k){
                     dist[v] = dist[u] + wt;
                     q.add(new Info(v, dist[v], curr.stops + 1));
                 }
             }
+        }
+        if(dist[dest] == Integer.MAX_VALUE){
+            return -1;
+        }else{
+            return dist[dest];
         }
     }
     public static void main(String[] args) {
         int n = 4;
         int[][] flights = {{0,1,100}, {1,2,100}, {2,0,100}, {1,3,600}, {2,3,200}};
         int src = 0, dest = 3, k = 1;
-
+        System.out.println(cheapestFlight(n,flights,src, dest, k));
     }
 }
