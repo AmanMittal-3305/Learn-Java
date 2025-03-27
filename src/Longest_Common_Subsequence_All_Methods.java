eccfb07894d3fb160260b37f979484e469f0a094
Aman Mittal <amanmittal3305@gmail.com>
2025-03-27 18:23:43 +0530

    Learning DP

---
+++ src/Longest_Common_Subsequence_All_Methods.java
@@ -0,0 +1,84 @@
+import java.util.*;
+public class Longest_Common_Subsequence_All_Methods {
+    public static int lcs(String str1, String str2, int n, int m){
+        if(n == 0 || m == 0){
+            return 0;
+        }
+        if(str1.charAt(n-1) == str2.charAt(m-1)) { //same characters at end of both strings
+            return 1 + lcs(str1, str2, n - 1, m - 1);
+        }else{ //different characters at end of both strings
+            int ans1 = lcs(str1, str2, n - 1, m);
+            int ans2 = lcs(str1, str2, n, m - 1);
+            return Math.max(ans1, ans2);
+        }
+    }
+
+    public static int lcs_memoization(int[][] dp, String str1, String str2, int n, int m){
+        if(n == 0 || m == 0){
+            return 0;
+        }
+        if(dp[n][m] != -1){
+            return dp[n][m];
+        }
+        if(str1.charAt(n-1) == str2.charAt(m-1)) { //same characters at end of both strings
+            return dp[n][m] = 1 + lcs_memoization(dp, str1, str2, n - 1, m - 1);
+        }else{ //different characters at end of both strings
+            int ans1 = lcs_memoization(dp, str1, str2, n - 1, m);
+            int ans2 = lcs_memoization(dp, str1, str2, n, m - 1);
+            return dp[n][m] = Math.max(ans1, ans2);
+        }
+    }
+
+    public static int lcs_tabulation(int[][] dp, String str1, String str2, int n, int m){
+        if(n == 0 || m == 0){
+            return 0;
+        }
+        if(dp[n][m] != -1){
+            return dp[n][m];
+        }
+        //Initialization
+        for(int i = 0; i < n+1; i++){
+            for(int j = 0; j < m+1; j++){
+                if(i == 0 || j == 0){
+                    dp[i][j] = 0;
+                }
+            }
+        }
+        for(int i = 1; i < n+1; i++){
+            for(int j = 1; j < m+1; j++){
+                if(str1.charAt(i-1) == str2.charAt(j-1)){
+                    dp[i][j] = 1 + dp[i-1][j-1];
+                }else{
+                    dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
+                }
+            }
+        }
+        print(dp);
+        return dp[n][m];
+    }
+
+    public static void print(int[][] dp){
+        for(int i = 0; i < dp.length; i++){
+            for(int j = 0; j < dp[0].length; j++){
+                System.out.print(dp[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        String str1 = "abcde";
+        String str2 = "ace";
+        int n = str1.length();
+        int m = str2.length();
+        int[][] dp = new int[n+1][m+1];
+        for(int i = 0; i < dp.length; i++){
+            for(int j = 0; j < dp[0].length; j++){
+                dp[i][j] = -1;
+            }
+        }
+//        System.out.println("Answer is : " + lcs(str1, str2, n, m));
+//        System.out.println("Answer is : " + lcs_memoization(dp, str1, str2, n, m));
+        System.out.println("Answer is : " + lcs_tabulation(dp, str1, str2, n, m));
+    }
+}
