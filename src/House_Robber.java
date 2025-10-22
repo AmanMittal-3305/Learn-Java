ff68b675e529680a31656939b484f4392d70f07b
Aman Mittal <amanmittal3305@gmail.com>
2025-10-22 22:22:24 +0530

    Solving Leetcode problems

---
+++ src/House_Robber.java
@@ -0,0 +1,24 @@
+public class House_Robber {
+    public int robDP(int n, int[] dp, int[] nums) {
+        // base case
+        if (n < 0) return 0;
+        if (n == 0) return nums[0];
+
+        // already computed
+        if (dp[n] != -1) return dp[n];
+
+        // choose to rob or skip
+        int pick = nums[n] + robDP(n - 2, dp, nums);
+        int skip = robDP(n - 1, dp, nums);
+
+        dp[n] = Math.max(pick, skip);
+        return dp[n];
+    }
+
+    public int rob(int[] nums) {
+        int n = nums.length;
+        int[] dp = new int[n];
+        Arrays.fill(dp, -1);
+        return robDP(n - 1, dp, nums);
+    }
+}
