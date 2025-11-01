e5604b476caae64b66180853cc8bf5695bbd0f73
Aman Mittal <amanmittal3305@gmail.com>
2025-11-01 10:40:30 +0530

    Solving Leetcode Problem

---
+++ src/Longest_Repeating_Character_Replacement.java
@@ -0,0 +1,21 @@
+public class Longest_Repeating_Character_Replacement {
+    public int characterReplacement(String s, int k) {
+        int[] count = new int[26];
+        int left = 0, maxCount = 0, maxLen = 0;
+
+        for (int right = 0; right < s.length(); right++) {
+            count[s.charAt(right) - 'A']++;
+            maxCount = Math.max(maxCount, count[s.charAt(right) - 'A']);
+
+            // if replacements needed > k, shrink window
+            while ((right - left + 1) - maxCount > k) {
+                count[s.charAt(left) - 'A']--;
+                left++;
+            }
+
+            maxLen = Math.max(maxLen, right - left + 1);
+        }
+
+        return maxLen;
+    }
+}
