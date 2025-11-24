54776e4f5666cf7206e0d77a25318091a624f302
Aman Mittal <amanmittal3305@gmail.com>
2025-11-24 22:06:01 +0530

    Solving Leetcode Problems

---
+++ src/Binary_Prefix_Divisible_By_5.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Binary_Prefix_Divisible_By_5 {
+    public List<Boolean> prefixesDivBy5(int[] nums) {
+        List<Boolean> res = new ArrayList<>();
+        int val = 0;
+
+        for (int n : nums) {
+            val = ((val << 1) + n) % 5;
+            res.add(val == 0);
+        }
+
+        return res;
+    }
+}
