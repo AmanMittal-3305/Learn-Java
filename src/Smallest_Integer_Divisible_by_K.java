fd70e3c6d422a535c90b747b891a5a4f5df84160
Aman Mittal <amanmittal3305@gmail.com>
2025-11-25 21:30:11 +0530

    Solving Leetcode Problems

---
+++ src/Smallest_Integer_Divisible_by_K.java
@@ -0,0 +1,11 @@
+public class Smallest_Integer_Divisible_by_K {
+    public int smallestRepunitDivByK(int k) {
+        if ( (k&1)==0|| k%5==0) return -1;
+        if (k==1) return 1;
+        int r=1;
+        for(int len0=2; ; len0++){
+            r=(10*r+1)%k;
+            if (r==0) return len0;
+        }
+    }
+}
